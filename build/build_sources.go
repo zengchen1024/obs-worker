b0cca9e8f7fe69d8fc92f2b2587053f10e1d3921
zengchen1024 <chenzeng2@huawei.com>
2022-05-17 14:28:39 +0800

    optimize the codes

--- build/build_sources.go
+++ build/build_sources.go
@@ -16,63 +16,75 @@ type buildSources struct {
 	*buildHelper
 }
 
-func (h *buildSources) getSource() (string, error) {
-	srcdir := h.getSrcdir()
+func (b *buildSources) getSource() (string, error) {
+	srcdir := b.getSrcdir()
 
 	if err := mkdir(srcdir); err != nil {
 		return "", err
 	}
 
-	info := h.info
+	info := b.getBuildInfo()
 
-	v, err := h.downloadPkgSource(
+	v, err := b.downloadPkgSource(
 		info.Project, info.Package, info.SrcMd5,
-		h.getSrcServer(), srcdir,
+		b.getSrcServer(), srcdir,
 	)
 	if err != nil {
 		return "", err
 	}
 
-	// verify sources
-	// TODO: the way to check is different to obs
-
-	keys := make([]string, len(v))
 	m := make(map[string]string)
 	for i := range v {
 		item := &v[i]
 		m[item.Name] = item.MD5
-		keys[i] = item.Name
 	}
 
-	sort.Strings(keys)
-
-	md5s := make([]string, 0, len(v))
-	for _, k := range keys {
-		if f := filepath.Join(srcdir, k); !isFileExist(f) {
-			return "", fmt.Errorf("%s is not exist", f)
-		}
-
-		md5s = append(md5s, genMetaLine(m[k], k))
-	}
-
-	md5 := utils.GenMD5([]byte(strings.Join(md5s, "\n") + "\n"))
-	if md5 != info.VerifyMd5 {
-		return "", fmt.Errorf(
-			"source verification fails, %s != %s",
-			md5, info.VerifyMd5,
-		)
+	if err := b.verify(srcdir, m, info.VerifyMd5); err != nil {
+		return "", err
 	}
 
 	return genMetaLine(info.VerifyMd5, info.Package), nil
 }
 
-func (h *buildSources) getBdeps(dir string) ([]string, error) {
-	if m := h.info.getkiwimode(); m != "image" && m != "product" {
+func (b *buildSources) verify(dir string, knowns map[string]string, verifyMd5 string) error {
+	fs := lsFiles(dir)
+	if len(fs) == 0 {
+		return fmt.Errorf("didn't download any files")
+	}
+
+	sort.Strings(fs)
+	s := make([]string, len(fs))
+
+	for i, name := range fs {
+		md5, ok := knowns[name]
+		if !ok {
+			return fmt.Errorf("unexpected file: %s", name)
+		}
+
+		s[i] = genMetaLine(md5, name)
+	}
+
+	md5 := utils.GenMD5([]byte(strings.Join(s, "\n") + "\n"))
+
+	if md5 != verifyMd5 {
+		return fmt.Errorf(
+			"source verification fails, %s != %s",
+			md5, verifyMd5,
+		)
+	}
+
+	return nil
+}
+
+func (b *buildSources) getBdeps(dir string) ([]string, error) {
+	info := b.getBuildInfo()
+
+	if m := info.getkiwimode(); m != "image" && m != "product" {
 		return nil, nil
 	}
 
-	endpoint := h.getSrcServer()
-	items := h.info.getSrcBDep()
+	endpoint := b.getSrcServer()
+	items := info.getSrcBDep()
 	meta := make([]string, len(items))
 
 	for i, item := range items {
@@ -81,7 +93,7 @@ func (h *buildSources) getBdeps(dir string) ([]string, error) {
 			return nil, err
 		}
 
-		_, err := h.downloadPkgSource(
+		_, err := b.downloadPkgSource(
 			item.Project, item.Package, item.SrcMd5,
 			endpoint, saveTo,
 		)
@@ -95,7 +107,7 @@ func (h *buildSources) getBdeps(dir string) ([]string, error) {
 	return meta, nil
 }
 
-func (h *buildSources) downloadPkgSource(project, pkg, srcmd5, endpoint, saveTo string) (
+func (b *buildSources) downloadPkgSource(project, pkg, srcmd5, endpoint, saveTo string) (
 	[]filereceiver.CPIOFileMeta, error,
 ) {
 	opts := source.ListOpts{
@@ -110,17 +122,17 @@ func (h *buildSources) downloadPkgSource(project, pkg, srcmd5, endpoint, saveTo 
 		return name, filepath.Join(saveTo, name), true, nil
 	}
 
-	return source.List(h.gethc(), endpoint, &opts, check)
+	return source.List(b.gethc(), endpoint, &opts, check)
 }
 
-func (h *buildSources) downloadSSLCert() error {
-	v, err := sslcert.List(h.gethc(), h.getSrcServer(), h.info.Project, true)
+func (b *buildSources) downloadSSLCert() error {
+	v, err := sslcert.List(b.gethc(), b.getSrcServer(), b.info.Project, true)
 	if err != nil || len(v) == 0 {
 		return err
 	}
 
 	return utils.WriteFile(
-		filepath.Join(h.getSrcdir(), "_projectcert.crt"),
+		filepath.Join(b.getSrcdir(), "_projectcert.crt"),
 		v,
 	)
 }
